3a3920c42825fa2f425b3e5ae1b485a0cca4f755
Fabien <fabien.hinault@free.fr>
2017-12-25 17:18:15 +0100

    début commands

--- commands.bash
+++ commands.bash
@@ -1,7 +1,24 @@
 
-echo toto
+git init bac_a_sable
+
+pushd bac_a_sable
+
+ls .git
+
+find .git/objects
+
+find .git/objects -type f
+
+echo 'je suis content d''assister au dej tech' | git hash-object -w --stdin
+
+find .git/objects -type f
+
+popd
+rm -rf bac_a_sable
 
 echo toto
+
+
 echo titi
 echo tata
 
